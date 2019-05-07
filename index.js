6f14dc3f0586597568bbac03ef62e8714b6525af
demoncowgirl <bethsalvatore2018@gmail.com>
2019-05-07 14:41:36 -0400

    index.js update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-// var app = express();
+var app = express();
 var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/dist'));
 app.listen(port, function() {
