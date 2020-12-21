be3c5f907a7ea289758a6026b133392f18316bc1
Demoncowgirl <bethsalvatore2018@gmail.com>
2020-12-20 18:34:30 -0600

    fixes alignment of card-deck

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var runSequence = require('run-sequence');
+var watch = require('watch');
 
 // task to copy all htm/html files from app directory to dist directory
 gulp.task('copyhtml', function() {
   return gulp.src('app/*.+(htm|html)')
 	.pipe(gulp.dest('dist/'))
- });
+});
 
 gulp.task('copyimages', function(){
   return gulp.src('app/images/*.+(png|jpg|gif)')
@@ -24,20 +25,17 @@ gulp.task('copyjs', function() {
 });
 
 // function to change sass code to css
-gulp.task('sass', function() {
+gulp.task('sass', function () {
 	return gulp.src('app/scss/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('dist/css/'))
 });
 
-gulp.task('watch', function() {
-	gulp.watch('app/*.+(htm|html)', gulp.series('copyhtml'));
+gulp.task('watch', function () {
+  gulp.watch('app/*.+(htm|html)', gulp.series('copyhtml'));
   gulp.watch('app/**/*.css', gulp.series('copycss'));
   gulp.watch('app/**/*.scss', gulp.series('sass'));
   gulp.watch('app/**/*.+(jpg|jpeg|png|gif)', gulp.series('copyimages'));
-
 });
 
-gulp.task('default', function(callback) {
-	runSequence(['sass', 'copyhtml', 'copycss'], callback)
-});
+gulp.task('default', gulp.series('copyhtml', 'copyimages', 'copycss', 'sass', 'copyjs', 'watch'));
